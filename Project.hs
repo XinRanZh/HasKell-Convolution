7ef2e87565b42db0c094eac6e01c177a3022d9b7
XinRa <zhangxr7@students.cs.ubc.ca>
2021-02-27 14:16:55 +0800

    Update Project.hs
    Comment added

--- Project.hs
+++ Project.hs
@@ -1,11 +1,28 @@
-import Codec.Picture
+-- The essence of Gaussian image blur, or image blur, is to convert a pixel of a point into a "weighted average value" 
+-- that approximates it and its surrounding pixels in a certain way. If we understand an image as a matrix,
+-- we can express this algorithm of converting a pixel into a weighted average of a pixel and its surrounding pixels as another matrix. 
+-- In other words, it can be understood as a matrix of weights.
 
+-- We now assume two matrices. A is a larger matrix that stores all the data in the picture. 
+-- We can understand that each value in matrix A is the greyscale of the picture at the corresponding point, 
+-- and B is the Gaussian filter we use, which is the weight of a point and its surrounding points in the process of generating new values
 
-imgConv :: Image PixelRGB8 -> Image Pixel8
-imgConv img = pixelMap rgbtoGrey
+-- For this, we introduce the concept of convolution
+-- To understand this algorithm, you must first understand the basic principle of convolution. Convolution is an operation that calculates two matrices.
 
-rgbtoGrey :: PixelRGB8 -> Pixel8
-rgbtoGrey (PixelRGB8 r g b) = (r + g + b) `div` 3
+-- Convolution
+
+-- The convolution is, up-side-down the B matrix and place it in a certain position of the A matrix, multiply the values ​​of the corresponding matrix positions of A and B one by one, and fill in the result obtained The new matrix corresponds to the middle position of the B matrix, and the B matrix is ​​translated to cover each position of the A matrix. In this process, new values ​​are continuously calculated and the new values ​​are filled into the corresponding place in the result matrix. This process is called convolution
+
+-- In order to easily calculate the B matrix we use, that is, the Gaussian image filter, it is an asymmetric matrix, so we can use correlation (which can be understood as the non-inverted B matrix version of the convolution) to replace the convolution operation
+
+-- Padding
+
+-- The next question is, when you move the B matrix in the A matrix, when the B matrix moves to the corner of A,
+-- how to deal with the problem that the B matrix crosses the corner of A?
+
+-- For the sake of simplicity, in this project, it is assumed that as long as the center of matrix B is still in matrix A, 
+-- the result of this time will be calculated, so that when calculating, all parts beyond the edge are recorded as 0
 
 
 get image filter xi yi xf yf
